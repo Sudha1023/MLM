f8a7300becc24b469ea4a1245fa6a42723b7293b
love-sonkar <loveboss806@gmail.com>
2024-01-19 19:22:04 +0530

    added help section all pages

---
+++ pages/helpsection/helpsection.js
@@ -0,0 +1,17 @@
+const dropdown = document.querySelector(".dropdown");
+const dropdownLink = document.querySelector(".dropdownlink");
+if (dropdown) {
+  dropdown.addEventListener("click", () => {
+    dropdownLink.classList.toggle("dropdownlinkToggle");
+  });
+}
+
+const menuBar = document.querySelector(".menu_sidebar2");
+
+const sidebar2 = document.querySelector(".help_sidebar");
+if (menuBar) {
+  menuBar.addEventListener("click", () => {
+    sidebar2.classList.toggle("opensidebar2");
+  });
+}
+console.log("hi")
